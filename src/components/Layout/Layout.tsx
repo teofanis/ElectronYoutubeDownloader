9a0fbc13b94312d42fc8dd7a6710ad0f7d668659
Teofanis Papadopulos <teos_97@hotmail.com>
2022-08-20 21:02:54 +0100

    Add Dark Mode

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,11 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 const Layout = ({ children }: LayoutProps) => {
-  return <div className="flex flex-wrap w-full">{children}</div>;
+  return (
+    <div className="flex flex-wrap w-full h-[100vh] bg-primary-red dark:bg-primary-black">
+      <div className="container mx-auto">{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.tsx'],
   theme: {
+    colors: {
+      'primary-red': '#FF0000',
+      'primary-black': '#282828',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
