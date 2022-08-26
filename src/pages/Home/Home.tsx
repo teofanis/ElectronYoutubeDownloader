f6fd157dd1f369792c43f29e86a323fa5ecdaa6f
Teofanis Papadopulos <teos_97@hotmail.com>
2022-08-26 11:10:51 +0100

    remove 100vh

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Downloader, Heading, Layout, Logo } from 'components';
 const Home = () => {
   return (
     <Layout>
-      <div className="flex flex-col h-[100vh] mt-[10%]">
+      <div className="flex flex-col mt-[10%]">
         <div className="flex items-center justify-center space-x-4">
           <Logo className="h-[125px]" />
           <Heading>Youtube Downloader</Heading>
