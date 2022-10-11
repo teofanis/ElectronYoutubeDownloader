76d1507aa99f2d7438f431e3f1daf46b77e15d14
Teofanis Papadopulos <teos_97@hotmail.com>
2022-10-11 23:16:02 +0100

    update base case

--- src/renderer/components/Pagination/__tests__/Pagination.test.tsx
+++ src/renderer/components/Pagination/__tests__/Pagination.test.tsx
@@ -4,6 +4,22 @@ import { Pagination } from 'renderer/components';
 
 describe('Pagination Test', () => {
   it('should render', () => {
-    expect(render(<Pagination>Test</Pagination>)).toBeTruthy();
+    expect(
+      render(
+        <Pagination
+          currentPage={0}
+          pageHandler={jest.fn}
+          paginator={{
+            page: 1,
+            perPage: 3,
+            prePage: null,
+            nextPage: null,
+            total: 0,
+            totalPages: 1,
+            data: [1, 2],
+          }}
+        />
+      )
+    ).toBeTruthy();
   });
 });
