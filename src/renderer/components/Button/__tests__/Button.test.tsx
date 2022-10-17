beaf670976257017b4a38a880f974db099a77ecb
Teofanis Papadopulos <teos_97@hotmail.com>
2022-10-17 13:46:23 +0100

    Button Unit Tests

--- src/renderer/components/Button/__tests__/Button.test.tsx
+++ src/renderer/components/Button/__tests__/Button.test.tsx
@@ -6,4 +6,28 @@ describe('Button Test', () => {
   it('should render', () => {
     expect(render(<Button>Test</Button>)).toBeTruthy();
   });
+
+  it('should render with text', () => {
+    const { container } = render(<Button>Test</Button>);
+    expect(container).toHaveTextContent('Test');
+  });
+
+  it('should render with text and className', () => {
+    const { container } = render(<Button className="bg-red-500">Test</Button>);
+    expect(container).toHaveTextContent('Test');
+    expect(container.querySelector('button')).toHaveClass('bg-red-500');
+  });
+
+  it('can be disabled', () => {
+    const { container } = render(<Button disabled>Test</Button>);
+    expect(container.querySelector('button')).toBeDisabled();
+  });
+
+  it('can be clicked', () => {
+    const onClick = jest.fn();
+    const { container } = render(<Button onClick={onClick}>Test</Button>);
+    expect(container.querySelector('button')).not.toBeDisabled();
+    container.querySelector('button')?.click();
+    expect(onClick).toBeCalled();
+  });
 });
