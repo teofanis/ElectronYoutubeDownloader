efc2b657fef0f27da59869f4be55c8c6a48099b2
Teofanis Papadopulos <teos_97@hotmail.com>
2022-10-17 13:39:52 +0100

    Arrow Icon tests

--- src/renderer/components/ArrowIcon/__tests__/ArrowIcon.test.tsx
+++ src/renderer/components/ArrowIcon/__tests__/ArrowIcon.test.tsx
@@ -5,5 +5,16 @@ import { ArrowIcon } from 'renderer/components';
 describe('ArrowIcon Test', () => {
   it('should render', () => {
     expect(render(<ArrowIcon type="left" />)).toBeTruthy();
+    expect(render(<ArrowIcon type="right" />)).toBeTruthy();
+  });
+
+  it('should render left arrow', () => {
+    const { container } = render(<ArrowIcon type="left" />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render right arrow', () => {
+    const { container } = render(<ArrowIcon type="right" />);
+    expect(container).toMatchSnapshot();
   });
 });
