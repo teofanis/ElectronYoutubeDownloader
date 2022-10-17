d1afaeddc690963e2cd10afc83bf8776bfc30bb6
Teofanis Papadopulos <teos_97@hotmail.com>
2022-10-17 13:46:29 +0100

    Cancel Button Unit Tests

--- src/renderer/components/CancelButton/__tests__/CancelButton.test.tsx
+++ src/renderer/components/CancelButton/__tests__/CancelButton.test.tsx
@@ -6,4 +6,24 @@ describe('CancelButton Test', () => {
   it('should render', () => {
     expect(render(<CancelButton text="test" />)).toBeTruthy();
   });
+
+  it('should render correctly with class and text', () => {
+    const { container } = render(
+      <CancelButton text="test" className="bg-red-500" />
+    );
+    expect(container).toHaveTextContent('test');
+    expect(container.querySelector('button')).toHaveClass(
+      'bg-primary-red text-white hover:bg-red-400'
+    );
+  });
+
+  it('can be clicked', () => {
+    const onClick = jest.fn();
+    const { container } = render(
+      <CancelButton text="test" onClick={onClick} />
+    );
+    expect(container.querySelector('button')).not.toBeDisabled();
+    container.querySelector('button')?.click();
+    expect(onClick).toBeCalled();
+  });
 });
